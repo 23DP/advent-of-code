8890e3259c321a3f19eaeda74cb78aca1dcbf4d2
DP <55746309+23DP@users.noreply.github.com>
2020-01-04 13:53:13 +0100

    Create day3_02.py

---
+++ day3/day3_02.py
@@ -0,0 +1,42 @@
+from typing import  Tuple, Set, Dict
+
+f = open("C:\\Users\\Danijela\\Desktop\\faks\\python\\input.txt", "r")
+
+if f.mode == "r":
+	first, second = f.readlines()
+
+
+def locations(zmijug: str) -> Dict[Tuple, int]:
+	x = y = steps = 0
+	visited = {}
+
+	for seg in zmijug.split(","):
+		direction = seg[0]
+		distance = int(seg[1:])
+
+		for _ in range(distance):
+			steps += 1
+			if direction == "U":
+				y += 1
+			elif direction == "D":
+				y -= 1
+			elif direction == "R":
+				x += 1
+			else:
+				x -= 1
+				
+			c = (x,y)
+			if c not in visited:
+				visited[c] = steps
+	
+	return visited
+
+def intersections(zmijug1: str, zmijug2: str) -> int:
+	locations1 = locations(zmijug1)
+	locations2 = locations(zmijug2)
+	inters = set(locations1.keys()).intersection(set(locations2.keys()))
+
+	return min(locations1[i] + locations2[i] for i in inters)
+
+print(intersections(first, second))
+f.close()
