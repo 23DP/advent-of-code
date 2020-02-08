301ae98d84bc578c1dd736ab984fc23576adc460
Dani <55746309+23DP@users.noreply.github.com>
2020-02-08 13:30:15 +0100

    Update day1_02.py

--- day1/day1_02.py
+++ day1/day1_02.py
@@ -1,11 +1,9 @@
 from numpy import floor
 
-def required_fuel(fuel: int) -> int:
-	if fuel < 7:
-		return 0
-	else:
-		return floor(fuel/3) -2 + required_fuel(floor(fuel/3) -2)
+def required_fuel(fuel: int):
+		return 0 if ( fuel < 7 ) else floor(fuel/3) -2 + required_fuel(floor(fuel/3) -2)
 
-def all_fuel(fuels: list) -> int:
-	with open(path, "r") as fuels:
+
+def all_fuel(fuels: list):
+	with open('input.txt') as fuels:
 		return sum(required_fuel(int(fuel)) for fuel in fuels.readlines())
