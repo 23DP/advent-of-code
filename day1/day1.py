328a212669ed15ce3226c504e6ad09c291cb84dc
Dani <55746309+23DP@users.noreply.github.com>
2020-02-08 13:28:03 +0100

    Update day1.py

--- day1/day1.py
+++ day1/day1.py
@@ -1,11 +1,4 @@
 from numpy import floor
-f = open(path, "r")
-
-if f.mode == "r":
-	input = f.readlines()
-
-def required_fuel(fuels: list) -> int:
-	#with open(path, "r") as fuels:
-	#return sum(floor(int(fuel) / 3) - 2 for fuel in fuels.readlines())
-	return sum(floor(int(fuel) / 3) - 2 for fuel in fuels)
-f.close()
+def required_fuel():
+	with open('input.txt') as fuels:
+		return sum( floor(int(fuel) / 3) - 2 for fuel in fuels.readlines() )
