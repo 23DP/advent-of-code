4578043c4f7f9eedc90ca08383688fd2b4a7a8d0
Dani <55746309+23DP@users.noreply.github.com>
2020-02-09 19:37:26 +0100

    Update day8_02.py

--- day8/day8_02.py
+++ day8/day8_02.py
@@ -15,8 +15,8 @@ for i in range(d3):
 
 finalPic = image[0] #initializing the picture with the first layer
 for layer in image:
-    for i in range(0, row):
-        for j in range(0, column):
+    for i in range(row):
+        for j in range(column):
             if finalPic[i][j] == 2:          #if the value of the pixel is 2, meaning transparent,
                 finalPic[i][j] = layer[i][j] #change it's value to the corresponding pixel in the current layer
 
